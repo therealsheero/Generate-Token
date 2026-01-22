bb3bd5fb2c30e187a1c83393a19ae78069c891af
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2026-01-22 15:39:45 +0530

    Delete backend/src/routes/otp.routes.js

--- backend/src/routes/otp.routes.js
+++
@@ -1,7 +0,0 @@
-// const router = require("express").Router();
-// const otp = require("../controllers/otp.controller");
-
-// router.post("/send-otp", otp.sendOTP);
-// router.post("/verify-otp", otp.verifyOTP);
-
-// module.exports = router;
