7ac14dc42b72feac6f69ff69d17fb33ff6b85aa3
Shreeya Pandey <131512383+therealsheero@users.noreply.github.com>
2026-01-27 11:07:05 +0530

    Update booking.routes.js

--- backend/src/routes/booking.routes.js
+++ backend/src/routes/booking.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-// const timeWindow = require("../middlewares/timeWindow.middleware");
 const bookingController = require("../controllers/booking.controller");
 const timeWindow = require("../middlewares/timeWindow.middleware");
 
